c628846b03560c051511b4e4e05cded03ff645e7
itzkrixh <krishna99405@gmail.com>
2023-03-22 10:24:24 +0530

    added array

--- firstcontract.sol
+++ firstcontract.sol
@@ -6,6 +6,17 @@ contract FirstContract {
     uint256 number;
     string name;
     mapping (string=>uint256) public stringtonumber;
+    People[] public people;
+
+    struct People {
+        uint256 id;
+        string name;
+        uint256 age;
+    }
+
+    function addPeople(string memory _name,uint256 _age) public {
+        people.push(People(people.length,_name,_age));
+    }
 
     function setNumber(uint256 _number) public {
         number=_number;
@@ -18,6 +29,7 @@ contract FirstContract {
     function setName(string memory _name,uint256 _age) public {
         name=_name;
         stringtonumber[_name]=_age;
+        people.push(People(people.length,_name,_age));
     }
 
     function getName() view public returns(string memory){
